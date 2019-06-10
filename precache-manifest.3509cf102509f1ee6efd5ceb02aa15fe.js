d3cab40f1c7eda9209aebca2f35687317235c477
Julia F <46096629+juliafercello@users.noreply.github.com>
2019-06-10 18:02:11 -0500

    Updates

---
+++ precache-manifest.3509cf102509f1ee6efd5ceb02aa15fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db760e9591257904a589bbf10980ee9f",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "3d063a79b1c64fc55e91",
+    "url": "/clicky-game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "3d063a79b1c64fc55e91",
+    "url": "/clicky-game/static/js/main.ddb81874.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
